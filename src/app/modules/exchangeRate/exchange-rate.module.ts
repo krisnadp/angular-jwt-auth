b27a5d041c255049260f1bc6db4c790a083d458b
krisnadp <krisna10permana@gmail.com>
2024-05-19 16:09:16 +0700

    fix: template length on dashboard container

---
+++ src/app/modules/exchangeRate/exchange-rate.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExchangeRateComponent } from './page/exchange-rate/exchange-rate.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ExchangeRateComponent
+  ]
+})
+export class ExchangeRateModule { }
